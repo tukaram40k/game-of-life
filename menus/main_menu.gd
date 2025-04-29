a0f4e083d21a0ced82e3a90b5ddbedb5206e7296
tukaram40k <tukaram40k@gmail.com>
2025-04-29 23:00:02 +0300

    added main menu button logic

---
+++ menus/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+# потом че-то пихну сюда
+func _on_controls_button_pressed() -> void:
+	pass
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
